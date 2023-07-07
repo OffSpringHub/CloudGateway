fc1f07715d2f7bf3171c4b98b693c2a639ae7ac6
anasmoaya <anassmoayaa@gmail.com>
2023-07-07 17:44:16 +0100

    initial commit

---
+++ src/test/java/com/offshoringhub/cloudgateway/CloudGatewayOffshoringhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offshoringhub.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayOffshoringhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
